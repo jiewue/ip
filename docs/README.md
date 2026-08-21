e516034d590b82f1cd5701fb4dc14c8b285b14c0
Jia Jie <ljiajie15@gmail.com>
2026-08-21 13:50:15 +0800

    Renamed chatbot to Happy

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Happy User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Happy.java
@@ -0,0 +1,17 @@
+/**
+ * Main entry point for the Happy chatbot application.
+ */
+public class Happy {
+    /**
+     * Starts the Happy chatbot program and prints the welcome banner.
+     *
+     * @param args Command line arguments (not used).
+     */
+    public static void main(String[] args) {
+        String banner = "  _    _   _   ___  ___  __   __\n"
+                + " | |  | | /_\\ | _ \\| _ \\ \\ \\ / /\n"
+                + " | __ | |/ _ \\|  _/|  _/  \\ V / \n"
+                + " |_||_|_/_/ \\_\\_|  |_|     |_|  \n";
+        System.out.println(banner);
+    }
+}
